3637229320d1f582261322f02ce50dc4a5e15b31
Kim Seoungwon <49578974+tmddnjs1533@users.noreply.github.com>
2022-12-08 00:37:07 +0900

    웹소켓적용

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,27 @@
-alert("hi")
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+const socket = new WebSocket(`ws://${window.location.host}`); // "ws://localhost:3000"
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server v");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("Just got this: ", message.data, " from the server");
+  const li = document.createElement("li");
+  li.textContent = message.data;
+  messageList.appendChild(li);
+});
+
+socket.addEventListener("close", () => {
+  console.log("DisConnected from Server x");
+});
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -1,12 +1,34 @@
 import express from "express";
+import WebSocket from "ws";
+import http from "http";
 
 const app = express();
 
-app.set("view engine","pug");
+app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname + "/public"))
-app.get("/", (req,res) => res.render("home"))
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`)
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-app.listen(3000, handleListen)
+// app.listen(3000, handleListen)
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser ");
+  sockets.push(socket);
+  socket.on("close", () => {
+    console.log("DisConnected from the Browser x");
+  });
+  socket.on("message", (message) => {
+    sockets.forEach((aSocket) => aSocket.send(message.toString("utf8")));
+  });
+});
+
+server.listen(3000, handleListen);
